d52ec959b424de485adca4a8903d7b29735accdd
ahmed <ahmedmahfouzjob@gmail.com>
2018-06-21 09:12:41 +0200

    add comments

--- upload.php
+++ upload.php
@@ -8,15 +8,15 @@ GOOD LUCK
 /-------------------------*/    
 require_once 'filer.php';
 
-$Filer  = new Filer;
+$Filer  = new Filer; //instance Filer class
 
-$Filer->setSize(7);
+$Filer->setSize(7); //size is in MB and the Default is 7MB
 
-$Filer->setType("image/jpeg","image/jpg","image/png");  
+$Filer->setType("image/jpeg","image/jpg","image/png");  //set any mimetype 
 
-$Filer->saveTo("ll");
+$Filer->saveTo();//pass a sting and default is "Filer save"
 
-$Filer->upload();
+$Filer->upload(); //execute uploading
 
 
 
